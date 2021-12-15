bf2afb23a77c50063ee90b554c319faf8b7b0ce9
Sathish Kratos <rsathishikumar3@gmail.com>
2021-12-16 05:16:25 +0530

    -

--- public/script.js
+++ public/script.js
@@ -18,7 +18,7 @@ document.getElementById('testForm').addEventListener('submit', e => {
   fetch(`/api/stock-prices?stock=${stock1}&stock=${stock2}&like=${checkbox}`)
     .then(res => res.json())
     .then(data => {
-      document.getElementById('jsonResult').innerText = JSON.stringify(data);
+      // document.getElementById('jsonResult').innerText = JSON.stringify(data);
       document.getElementById('jsonResult').innerText = `{\n    stockData:[\n\t{\n\t    stock: ${data.stockData[0].stock},\n\t    price: ${data.stockData[0].price},\n\t    rel_likes: ${data.stockData[0].rel_likes}\n\t},\n\t{\n\t    stock: ${data.stockData[1].stock},\n\t    price: ${data.stockData[1].price},\n\t    rel_likes: ${data.stockData[1].rel_likes}\n\t}\n    ]\n}`;
     });
 });
